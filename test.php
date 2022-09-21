fb2d3892dcf9f2bca125b94a1922f9b7bd6a77db
Cesar Arturo Martinez Hernandez <cesar.arthur.mtz.hdz@gmail.com>
2022-09-21 14:37:34 -0500

    Se agrega el commit con el index.php y el test, en base a los archivos proporcionados para la práctica

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Tests</title>
+        <link rel="stylesheet" href="../../css/styles.css">
+    </head>
+    <body>
+        <!-- Make a form for user data -->
+        <form action="../../index.html" method="post">
+            <input type="text" name="name" placeholder="Name">
+            <input type="text" name="name" placeholder="Name">
+                    <input type="text" name="surname" placeholder="Phone number">
+            <input type="submit" value="Send">
+        </form>
+        <!-- Show the result of the form -->
+        <p>Helleo, <?php echo $_POST['name']; ?>!</p>
+    </body>
+</html>
